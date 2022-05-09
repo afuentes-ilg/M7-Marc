b434e4c5194f95ae12bad316997a88ff46850c4a
afuentes-ilg <74978866+afuentes-ilg@users.noreply.github.com>
2022-05-09 19:29:13 +0200

    práctica acabada

--- app.js
+++ app.js
@@ -22,4 +22,20 @@ app.get("/products", (req,res) => {
 
   res.json(products);
 })
+
+app.get('/users', (req,res) => {
+    res.json([{
+      id: 1,
+      name: 'User Userson'
+    }])
+  })
+  
+  app.get('/products', (req, res) => {
+    res.json([{
+      id: 1,
+      name: 'The Bluest Eye'
+    }])
+  })
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
